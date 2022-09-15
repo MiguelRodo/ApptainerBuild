b2a0b2f7782378db0417fe96f053fa307ff9ed43
Miguel Julio Rodo <rdxmig002@myuct.ac.za>
2022-09-15 17:08:37 +0200

    Ad ar36.sif transfer lines

--- scripts/transfer.sh
+++ scripts/transfer.sh
@@ -1,4 +1,6 @@
+cp ar36.sif "/mnt/h/Shared drives/StoreContainer/ar36.sif"
 cp ar41.sif "/mnt/h/Shared drives/StoreContainer/ar41.sif"
 cp ar42.sif "/mnt/h/Shared drives/StoreContainer/ar42.sif"
+cp ar36.sif $HOME/sif/ar36.sif
 cp ar41.sif $HOME/sif/ar41.sif
 cp ar42.sif $HOME/sif/ar42.sif
