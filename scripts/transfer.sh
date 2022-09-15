aaa3048ad06ff2b645640dd010a68c73ab94fb8c
Miguel Julio Rodo <rdxmig002@myuct.ac.za>
2022-09-15 16:42:27 +0200

    Add commands to transfer sif files

---
+++ scripts/transfer.sh
@@ -0,0 +1,4 @@
+cp ar41.sif "/mnt/h/Shared drives/StoreContainer/ar41.sif"
+cp ar42.sif "/mnt/h/Shared drives/StoreContainer/ar42.sif"
+cp ar41.sif $HOME/sif/ar41.sif
+cp ar42.sif $HOME/sif/ar42.sif
