afceec419bce7a995d797fa3c0591731b20ad55b
Miguel Julio Rodo <rdxmig002@myuct.ac.za>
2022-11-01 09:29:59 +0200

    Make 4.2 and 4.1 builds copy only on success

--- scripts/build-ar41.sh
+++ scripts/build-ar41.sh
@@ -1,4 +1,5 @@
-apptainer build -F r41-plain.sif r41.def
-apptainer build -F ar41.sif r41-add_vsc.def
-cp ar41.sif "/mnt/h/Shared drives/StoreContainer/ar41.sif"
-cp ar41.sif $HOME/sif/ar41.sif
+apptainer build -F r41-plain.sif r41.def \
+  && apptainer build -F ar41.sif r41-add_vsc.def \
+  && cp ar41.sif $HOME/sif/ar41.sif \
+  && cp ar41.sif "/mnt/h/Shared drives/StoreContainer/ar41.sif" 
+
