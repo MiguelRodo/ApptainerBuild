a1a28d46da7aaca6932433c5e2af36c134822f70
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-06-07 12:09:38 +0100

    Correct ubuntu r setup file

--- src/docker/ubuntu/R/add_r_libs-select.sh
+++ src/docker/ubuntu/R/add_r_libs-select.sh
@@ -1,20 +1,20 @@
-sudo apt-get install -y |
-  fonts-roboto |
-  libcairo2-dev |
-  libclang-dev |
-  libcurl4-openssl-dev |
-  libfontconfig1-dev |
-  libfreetype6-dev |
-  libgit2-dev |
-  libglpk40 |
-  libicu-dev |
-  libicu[0-9][0-9] |
-  libopenssl-dev |
-  libssl-dev |
-  libstdc++6 |
-  libudunits2-dev |
-  libxml2-dev |
-  libxt-dev |
-  libsodium-dev |
-  locales |
+apt-get install -y \
+  fonts-roboto \
+  libcairo2-dev \
+  libclang-dev \
+  libcurl4-openssl-dev \
+  libfontconfig1-dev \
+  libfreetype6-dev \
+  libgit2-dev \
+  libglpk40 \
+  libicu-dev \
+  libicu[0-9][0-9] \
+  libsodium-dev \
+  libssl-dev \
+  libstdc++6 \
+  libudunits2-dev \
+  libxml2-dev \
+  libxt-dev \
+  locales \
   zlib1g
+  
