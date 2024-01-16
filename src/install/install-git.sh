2023c3f2b0b06356ca256efe83b317a042dfe80e
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2024-01-16 09:18:43 +0000

    Update Git in all R versions

--- src/install/install-git.sh
+++ src/install/install-git.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+set -e 
 apt-get update
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A1715D88E1DF1F24
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:git-core/ppa
 apt update
--- src/install/install-xet.sh
+++ src/install/install-xet.sh
@@ -7,13 +7,8 @@
 set -e
 
 pushd /tmp
-
 wget https://github.com/xetdata/xet-tools/releases/latest/download/xet-linux-x86_64.deb
-
 apt-get install -y ./xet-linux-x86_64.deb
-
 git xet install
-
 rm -rf ./xet-linux-x86_64.deb
-
 popd
