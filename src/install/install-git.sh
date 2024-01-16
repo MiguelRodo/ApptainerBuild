15fdc9878824bf12846d8cf9dc1ddad00e525f36
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2024-01-16 09:24:01 +0000

    Use apt-get instead of apt

--- src/install/install-git.sh
+++ src/install/install-git.sh
@@ -4,5 +4,5 @@ apt-get update
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A1715D88E1DF1F24
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:git-core/ppa
-apt update
-apt install git
+apt-get update
+apt-get install -y git
