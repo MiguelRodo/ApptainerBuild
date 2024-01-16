94ee27ca2537aa5fbdeb19d0fe3d70787ce96215
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2024-01-16 09:30:28 +0000

    Upgrade before installing Git

--- src/install/install-git.sh
+++ src/install/install-git.sh
@@ -5,4 +5,5 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A1715D88E1DF1F24
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:git-core/ppa
 apt-get update
+apt-get upgrade -y
 apt-get install -y git
