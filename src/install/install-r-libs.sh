7abcde4a653d897c32df54e241c121a44fd89f13
Miguel Rodo <rdxmig002@myuct.ac.za>
2024-01-24 17:26:44 +0200

    Move more packages into non-R3.6.3

--- src/install/install-r-libs.sh
+++ src/install/install-r-libs.sh
@@ -7,22 +7,23 @@ apt-get install -y \
   fonts-roboto \
   libcairo2-dev \
   libclang-dev \
-  libcurl4-openssl-dev \
   libfontconfig1-dev \
   libfreetype6-dev \
-  libgit2-dev \
-  libglpk40 \
   libicu-dev \
   libicu[0-9][0-9] \
-  libsodium-dev \
   libssl-dev \
   libstdc++6 \
-  libudunits2-dev \
   libxml2-dev \
   libxt-dev \
   locales \
   zlib1g
 
 if ! [ "$1" == 36 ]; then
-  apt-get install -y imagemagick
+  apt-get install -y \
+    imagemagick \
+    libcurl4-openssl-dev \
+    libgit2-dev \
+    libglpk40 \
+    libsodium-dev \
+    libudunits2-dev
 fi
