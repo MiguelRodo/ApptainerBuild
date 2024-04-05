125b0c7f4c1d87d9250208053072e0e718cda3fc
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2024-04-05 11:05:33 +0100

    Ensure gnupg installed before using apt-key

--- src/install/install-git.sh
+++ src/install/install-git.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e 
+apt-get clean
+rm -rf /var/lib/apt/lists/*
 apt-get update
+apt-get install -y gnupg gnupg2 gnupg1
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A1715D88E1DF1F24
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:git-core/ppa
